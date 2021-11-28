67f07692bc7cbdf3ebe6661c31ce34a231af80d2
MisterMAX789 <93163193+MisterMAX789@users.noreply.github.com>
2021-11-28 20:04:13 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int startBalance = 100;
+        int add = 1700;
+        int bonus;
+        if (add > 1000) {
+            bonus = (int) add / 100;
+        } else {
+            bonus = 0;
+        }
+        startBalance += add + bonus;
+        System.out.println(bonus);
+        System.out.println(startBalance);
+        }
+    }
